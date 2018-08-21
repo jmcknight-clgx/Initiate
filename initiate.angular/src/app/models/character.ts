4fe3d9a1c73efbd5742ede1a49af447e1ac94ea7
Jeremy McKnight <jmcknight@corelogic.com>
2018-08-21 15:31:10 -0500

    Added HP to character form
    Signed-off-by: Sarah Odom <sarahkateodom@gmail.com>

--- initiate.angular/src/app/models/character.ts
+++ initiate.angular/src/app/models/character.ts
@@ -4,6 +4,7 @@ export class Character {
     public name: string;
     public initiative: number;
     public ac: number;
+    public hp: number;
     public statuses: Status[];
     public isSelected: boolean;
 
@@ -11,6 +12,7 @@ export class Character {
         this.name = character.name;
         this.initiative = character.initiative;
         this.ac = character.ac;
+        this.hp = character.hp;
         this.statuses = character.statuses;
         this.isSelected = false;
     }
